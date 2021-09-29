2daa272f044cb58d5b7c3e0ad154730422e778f9
tejas0207 <67390503+tejas0207@users.noreply.github.com>
2021-09-29 13:19:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -140,4 +140,4 @@ def after():
     # return send_file(out, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(debug=True, port=4568)
+    app.run(host='0.0.0.0', debug=True, port=8080)
